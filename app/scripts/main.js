f0da1de8348b84e5e628a3fefc5ce56e9eb708f7
Yves Benchimol <yves.benchimol@polytechnique.org>
2014-08-02 10:53:21 -0700

    structure of LnB

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+
+    var maskIsShown = true;
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 100 && maskIsShown) {
+            $('.header-background').removeClass("bg");
+            maskIsShown = false;
+        }
+        else if ($(window).scrollTop() < 100 && !maskIsShown) {
+            $('.header-background').addClass("bg");
+            maskIsShown = true;
+        }
+    });
+
+    $('.header-background ul li a').click(function(e){
+        e.preventDefault();
+        var target = $(this).attr('href');
+        var elem = $(target);
+        var offsetTop = elem.offset().top;
+        var body = $("html, body");
+        body.animate({
+            scrollTop:offsetTop
+        }, '500', 'swing');
+    });
+
+});
