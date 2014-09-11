48f2f3b83bf5b6f01ed5371df2667da314499ffc
yvobenchi <yves.benchimol@polytechnique.org>
2014-09-10 17:43:27 -0700

    autoplay +resizing btn

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,24 +2,28 @@ $(document).ready(function(){
     $("#nav").scroll_navi();
     $('.product-carousel').slick({
     	arrows: false,
+    	autoplay: true,
+    	autoplaySpeed: 4000,
+    	pauseOnDotsHover: true,
+    	speed: 400,
     	dots: true
 	});
 	$( ".slick-dots" ).find("li button").hide();
 	$( ".slick-dots" ).find("li").each(function(i){
 		if(i==0){
-			$(this).append(" <i class='fa fa-map-marker fa-3x'></i>");
+			$(this).append(" <i class='fa fa-map-marker fa-2x'></i>");
 			$(this).append(" <div class='picto-txt'>Find</div> ");
 		}
         else if(i==1){
-			$(this).append(" <i class='fa fa-line-chart fa-3x'></i>");
+			$(this).append(" <i class='fa fa-line-chart fa-2x'></i>");
 			$(this).append(" <div class='picto-txt'>Improve</div> ");
 		}
 		else if(i==2){
-			$(this).append(" <i class='fa fa-eye fa-3x'></i>");
+			$(this).append(" <i class='fa fa-eye fa-2x'></i>");
 			$(this).append(" <div class='picto-txt'>Visualize</div> ");
 		}
 		else{
-			$(this).append(" <i class='fa fa-users fa-3x'></i>");
+			$(this).append(" <i class='fa fa-users fa-2x'></i>");
 			$(this).append(" <div class='picto-txt'>Share</div> ");
 		}
     });
